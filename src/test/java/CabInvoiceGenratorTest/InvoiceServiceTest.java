4a1e15043042b5de829d8a07694f664162e354e9
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-14 17:04:09 +0530

    [Nikhil] - added minimum fare condition

--- src/test/java/CabInvoiceGenratorTest/InvoiceServiceTest.java
+++ src/test/java/CabInvoiceGenratorTest/InvoiceServiceTest.java
@@ -15,4 +15,17 @@ public class InvoiceServiceTest {
         Assert.assertEquals(50.0,totalFare,0.1);
 
     }
+
+    @Test
+    public void givenLessDistanceOrTime_ReturnMinFare() {
+        CabInvoice cabInvoice = new CabInvoice();
+        double distanceInKm = 0.1;
+        double timeInMin = 0.1;
+        double totalFare = cabInvoice.totalFare(distanceInKm, timeInMin);
+        double minimumFare = 5.0;
+        double minFare = Math.max(totalFare,minimumFare);
+        Assert.assertEquals(5, minFare, 0.1);
+    }
+
+    
 }
