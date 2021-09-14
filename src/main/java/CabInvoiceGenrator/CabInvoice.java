8016f7877b30618696cd97f3b86253e3af705306
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-14 16:58:25 +0530

    [Nikhil] - added total fare for a ride

--- src/main/java/CabInvoiceGenrator/CabInvoice.java
+++ src/main/java/CabInvoiceGenrator/CabInvoice.java
@@ -2,4 +2,11 @@ package CabInvoiceGenrator;
 
 public class CabInvoice {
 
+    public static final double costPerTime = 1.0;
+    public static final double costPerKm = 4.0;
+
+    public double totalFare(double distanceInKm, double timeInMin){
+        double totalPrice = distanceInKm*costPerKm + timeInMin*costPerTime;
+        return totalPrice;
+    }
 }
